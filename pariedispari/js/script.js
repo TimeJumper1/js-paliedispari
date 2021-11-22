a04c74b67621b43ff37e9fa943bc4934d5665226
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-11-22 15:38:52 +0100

    codice a parole

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -1,1 +1,12 @@
-alert('test')
+// creare il gioco pari e dispari
+// utente sceglie pari o dispari
+// verificare la scelta?
+// utente sceglie un numero
+
+
+
+
+// funzione
+// sommare il numero dell'utente ad un numero generato casualmente dal computer
+// verificare se il numero è pari o dispari
+// mandare il relativo messaggio all'utente

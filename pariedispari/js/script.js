e38fca18b4324f2174bd2068e59793a0069523c5
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-11-22 16:30:29 +0100

    pulizia codice

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -10,12 +10,14 @@ console.log(userChoice);
 userNumber = parseInt(prompt('scegli un numero fra 1 e 5 (inclusi)'))
 console.log(userNumber);
 
-
 // funzione
 function evenOrOdd(number){
     let result
+    // numero casuale dell pc
     let pcNumber = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+    // sommare il numero dell'utente ad un numero generato casualmente dal computer
     resultNumber = pcNumber + number ;
+    // verificare se il numero è pari o dispari
     if (resultNumber % 2 === 0){
         result = 'pari'
     }else {
@@ -23,9 +25,10 @@ function evenOrOdd(number){
     }
     return result
 }
-// test
+
 
 test = evenOrOdd(userNumber)
+// mandare il relativo messaggio all'utente
 if(userChoice === test){
     alert('congratulazioni hai vinto')
 }else {
@@ -33,6 +36,5 @@ if(userChoice === test){
 }
 
 
-// sommare il numero dell'utente ad un numero generato casualmente dal computer
-// verificare se il numero è pari o dispari
-// mandare il relativo messaggio all'utente
+
+

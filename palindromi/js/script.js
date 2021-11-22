d6bfe656c588374649bc840e5ec64a82e3c15017
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-11-22 15:33:31 +0100

    palindromi completato

---
+++ palindromi/js/script.js
@@ -0,0 +1,28 @@
+// verificare se una stringa dell'utente sia palindroma
+userWord = prompt('inserisci la parola da verificare');
+console.log(userWord);
+
+
+
+
+// funzione di verifica
+function verificaPalindromi(word){
+    let result;
+    let drow = '';
+    for (i = (word.length-1); i >= 0; i--) {
+        console.log(word[i]);
+        drow +=word[i];
+        
+    }
+    console.log(drow);
+    if (word === drow){
+        result = 'la parola è un palindromo';
+    }else {
+        result = 'la parola non è un palindromo';
+    }
+    return result;
+}
+
+
+test = verificaPalindromi(userWord);
+alert(test);
